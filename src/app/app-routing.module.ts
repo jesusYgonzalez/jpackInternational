51d6fdfe98491644d1e0d523d58988e4aec3a131
jesus gonzalez <jesusgonzalez@chuy.local>
2019-07-02 02:00:39 -0700

    created the routing for the top navbar items with links, also worked on about us page and contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './navbar/home/home.component';
+import { AboutComponent } from './navbar/about/about.component';
+import { ContactUsComponent } from './navbar/contact-us/contact-us.component';
+import { DeliveryComponent } from './navbar/delivery/delivery.component';
+import { OrderOnlineComponent } from './navbar/order-online/order-online.component';
+import { QuotationComponent } from './navbar/quotation/quotation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'delivery', component: DeliveryComponent },
+  { path: 'order', component: OrderOnlineComponent },
+  { path: 'quotation', component: QuotationComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
